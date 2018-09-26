0b94c2d03bb2cff82b18f2a636296d462e0165b3
Moha Bahusayn <bahusayn7@gmail.com>
2018-09-26 20:01:01 +0300

    CodeMaid the code.

--- SchoolSystem/Models/SchoolDBContext.cs
+++ SchoolSystem/Models/SchoolDBContext.cs
@@ -16,5 +16,24 @@ namespace SchoolSystem.Models
         public DbSet<Subject> Subjects { get; set; }
         public DbSet<Student> Students { get; set; }
         public DbSet<Instructor> Instructors { get; set; }
+
+        //public static SchoolDBContext Create()
+        //{
+        //    return new SchoolDBContext();
+        //}
+
+        //protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        //{
+        //    base.OnModelCreating(modelBuilder);
+        //    modelBuilder.Entity<Classroom>().ToTable("Classrooms");
+        //    modelBuilder.Entity<Subject>().ToTable("Subjects");
+        //    modelBuilder.Entity<Student>().ToTable("Students");
+        //    modelBuilder.Entity<Instructor>().ToTable("Instructors");
+        //}
+
+        //public DbQuery<T> Query<T>() where T : class
+        //{
+        //    return Set<T>().AsNoTracking();
+        //}
     }
 }
